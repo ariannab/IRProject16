7142592f1ce72037453db186e53c074ed2e9235d
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-01-05 10:09:40 +0100

    Factory for building the analyzers

---
+++ src/main/java/analyzers/CustomAnalyzerFactory.java
@@ -0,0 +1,33 @@
+package analyzers;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+
+public class CustomAnalyzerFactory {
+
+	public static CustomAnalyzer buildTweetAnalyzer() throws IOException {
+		String urlRegex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+		CustomAnalyzer analyzer = CustomAnalyzer.builder()
+				.addCharFilter("patternreplace", "pattern",
+						urlRegex, "replacement", "")
+				.withTokenizer("lowercase").addTokenFilter("length", "min", "3", "max", "100")
+				.addTokenFilter("stop", "words",
+						"mysql_stopwords.txt", "format",
+						"wordset")
+				.addTokenFilter("englishminimalstem")
+				.build();
+		return analyzer;
+	}
+
+	public static CustomAnalyzer buildNewsAnalyzer() throws IOException {
+		CustomAnalyzer analyzer = CustomAnalyzer.builder()
+				.withTokenizer("lowercase").addTokenFilter("length", "min", "3", "max", "100")
+				.addTokenFilter("stop", "words",
+						"mysql_stopwords.txt", "format",
+						"wordset")
+				.addTokenFilter("englishminimalstem")
+				.build();
+		return analyzer;
+	}
+}
