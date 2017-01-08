5e625609d99a86a74c5dfa81748844f2892b066a
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-01-08 20:34:39 +0100

    Minor fixings for custom analyzer

--- src/main/java/analyzers/CustomAnalyzerFactory.java
+++ src/main/java/analyzers/CustomAnalyzerFactory.java
@@ -11,23 +11,24 @@ public class CustomAnalyzerFactory {
 		CustomAnalyzer analyzer = CustomAnalyzer.builder()
 				.addCharFilter("patternreplace", "pattern",
 						urlRegex, "replacement", "")
-				.withTokenizer("lowercase").addTokenFilter("length", "min", "3", "max", "100")
+				.withTokenizer("lowercase")
 				.addTokenFilter("stop", "words",
 						"long_stopwords.txt", "format",
 						"wordset")
 				.addTokenFilter("porterstem")
+				.addTokenFilter("length", "min", "3", "max", "15")
 				.build();
 		return analyzer;
 	}
 
-	public static CustomAnalyzer buildNewsAnalyzer() throws IOException {
-		CustomAnalyzer analyzer = CustomAnalyzer.builder()
-				.withTokenizer("lowercase").addTokenFilter("length", "min", "3", "max", "100")
-				.addTokenFilter("stop", "words",
-						"mysql_stopwords.txt", "format",
-						"wordset")
-				.addTokenFilter("porterstem")
-				.build();
-		return analyzer;
-	}
+//	public static CustomAnalyzer buildNewsAnalyzer() throws IOException {
+//		CustomAnalyzer analyzer = CustomAnalyzer.builder()
+//				.withTokenizer("lowercase").addTokenFilter("length", "min", "3", "max", "100")
+//				.addTokenFilter("stop", "words",
+//						"mysql_stopwords.txt", "format",
+//						"wordset")
+//				.addTokenFilter("porterstem")
+//				.build();
+//		return analyzer;
+//	}
 }
