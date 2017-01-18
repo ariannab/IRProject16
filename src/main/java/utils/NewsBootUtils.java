f7ac21c2a4d333715f3125fab63ad6eda5f76cb7
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-01-18 19:51:21 +0100

    Refactoring (including Javadoc) of NewsBootUtils

--- src/main/java/utils/NewsBootUtils.java
+++ src/main/java/utils/NewsBootUtils.java
@@ -24,6 +24,16 @@ import model.RespSources;
 import model.Source;
 
 public class NewsBootUtils {
+	
+	/**
+	 * Submit GET Http Request to NewsAPI in order to get 
+	 * all the sources IDs which language is English
+	 * 
+	 * @return set of Strings representing sources IDs
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 * @throws ProtocolException
+	 */
 	public static Set<String> getSourcesIDs() throws MalformedURLException, IOException, ProtocolException {
 		URL obj = new URL("https://newsapi.org/v1/sources?language=en");
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -47,6 +57,13 @@ public class NewsBootUtils {
 		return null;
 	}
 
+	/**
+	 * Obtain the response from NewsAPI 
+	 * 
+	 * @param con
+	 * @return the response, stored as StringBuffer
+	 * @throws IOException
+	 */
 	public static StringBuffer obtainResponse(HttpURLConnection con) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
@@ -59,6 +76,12 @@ public class NewsBootUtils {
 		return response;
 	}
 
+	/**
+	 * Format the JSON obtained
+	 * 
+	 * @param string
+	 * @return the JSON string nicely formatted
+	 */
 	public static String formatJson(String string) {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		JsonParser jp = new JsonParser();
@@ -67,18 +90,36 @@ public class NewsBootUtils {
 		return prettyJsonString;
 	}
 
+	/**
+	 * Build a RespArticles object from their JSON representation 
+	 * 
+	 * @param prettyJsonString
+	 * @return the RespArticles object
+	 */
 	public static RespArticles buildRespPOJO(String prettyJsonString) {
 		Gson gson = new Gson();
 		RespArticles resp = gson.fromJson(prettyJsonString, RespArticles.class);
 		return resp;
 	}
 
+	/**
+	 * Build a RespSources object from their JSON representation 
+	 * 
+	 * @param prettyJsonString
+	 * @return the RespSources object
+	 */
 	private static RespSources buildSourcePOJO(String prettyJsonString) {
 		Gson gson = new Gson();
 		RespSources resp = gson.fromJson(prettyJsonString, RespSources.class);
 		return resp;
 	}
 
+	/**
+	 * Read the NewsAPI key from file
+	 * 
+	 * @return the key
+	 * @throws FileNotFoundException
+	 */
 	public static String loadAPIKey() throws FileNotFoundException {
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		File file = new File(classloader.getResource("newsKey.txt").getFile());
@@ -92,7 +133,18 @@ public class NewsBootUtils {
 		}
 	}
 	
-	public static Set<Article> getAllArticlesFromSource(String sourceID, int j) throws MalformedURLException, IOException, ProtocolException {
+	
+	/**
+	 * Obtain through GET Http request all the articles from
+	 * a certain source
+	 * 
+	 * @param sourceID
+	 * @return the set of Article objects
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 * @throws ProtocolException
+	 */
+	public static Set<Article> getAllArticlesFromSource(String sourceID) throws MalformedURLException, IOException, ProtocolException {
 		String YOUR_APIKEY = NewsBootUtils.loadAPIKey();
 		
 		URL obj = new URL("https://newsapi.org/v1/articles?source="+sourceID+"&apiKey="+YOUR_APIKEY);
